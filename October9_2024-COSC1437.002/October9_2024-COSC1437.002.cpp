6dcb72027370eeaf4999cce26c3cacf74da9f901
Work <Work@MSI>
2024-10-09 16:26:49 -0500

    Add project files.

---
+++ October9_2024-COSC1437.002/October9_2024-COSC1437.002.cpp
@@ -0,0 +1,107 @@
+// October9_2024-COSC1437.002.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+
+#include"iomanip"
+
+using std::cout; 
+using std::string; 
+using std::setprecision; 
+
+class Person
+{
+public: 
+    string firstName; 
+    string lastName; 
+    int age = 11;
+
+    void print() const
+    {
+        cout << "first name: " << firstName << "\n";
+        cout << "last name: " << lastName << "\n";
+        cout << "age: " << age << "\n";
+    }
+
+    /*Our implementation of a default Person constructor*/
+    Person()
+    {
+        firstName = "first name TBD";
+        lastName = "last name TBD";
+    }
+};
+class A
+{
+
+};
+
+class Employee : public Person//, A //; NO! (Maybe multiple inheritance isn't such a good idea (hard to understand) 
+{
+public: 
+    string jobTitle; 
+    double salary; 
+
+    //let's write an Employee default constructor and a parameterized constructor 
+    Employee()
+        :Person() //what is this?
+    {
+        jobTitle = "Job title to be determined";
+        salary = 9.90; 
+    }
+
+    void print() const // override -> we will talk about this more when we get to "abstract" classes 
+    {
+        //jobTitle = "adsfasdf";
+        Person::print(); //this will print the first three attributes of an Employee 
+
+        cout << "Job title: " << jobTitle << "\n";
+        cout << "Salary: $" << std::fixed << setprecision(2) << salary << "\n";
+    }
+};
+
+class Manager : public Employee // a manager "is-a(n)" Employee
+{
+public: 
+    std::vector<Person> peopleManaged; //ex = { "Alice", "Bob", "Carol" };
+
+};
+
+
+//another very typical example of inheritance: 
+class Shape
+{
+    double area; 
+};
+
+class Triangle : public Shape //when inheritance is used, it implies an "is-a" relationship 
+{
+
+};
+
+class Tail
+{
+    int tailWidth; 
+    bool isBushy; 
+};
+
+class Dog //: public Tail //No! A dog is not a tail -> a dog is COMPOSED of a tail 
+{
+    Tail tailObject; //this says that a Dog "has-a" tail
+};
+
+int main()
+{
+    Person you; 
+   
+    cout << "Person object attributes:\n";
+    you.print(); 
+
+    Employee alsoYou;; 
+    cout << "\n\n\nEmployee object attributes (using the default constructor): \n";
+    alsoYou.print(); //this function OVERRODE its parent!
+
+
+
+    //std::cout << "Hello World!\n";
+}
